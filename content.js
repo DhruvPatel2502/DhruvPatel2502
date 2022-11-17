13eeb11e46e8d98a188e6deeb0be64f28beff770
Isabel <acev0010@algonquinlive.com>
2022-11-17 13:02:22 -0500

    added the popup.html and changed something in the manifest.json

--- content.js
+++ content.js
@@ -1,6 +1,4 @@
 
-
-
 // const labels = document.getElementsByTagName("label");
 
 // for(label of labels) {
@@ -10,4 +8,12 @@
     for( var i = 0; i < labels.length; i++ ) {
        if (labels[i].getAttribute("for")){
        labels[i].style.border = "4px solid red";}
+       
     }
+let button = document.querySelector('#btn2');
+let msg = document.querySelector('#message');
+
+button.addEventListener('click', ()=>{
+  msg.classList.toggle('reveal');
+})
+
